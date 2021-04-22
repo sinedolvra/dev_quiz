69fb3c5b7ecf17b50e3e7d8bc1e12abc367eec32
Denis Oliveira <denis.olvra@gmail.com>
2021-04-22 14:52:13 -0300

    Add models

---
+++ lib/shared/models/quiz_model.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:dev_quiz/shared/models/question_model.dart';
+
+enum Level { facil, medio, dificil, perito }
+
+extension LevelStringExt on String {
+  Level get parse => {
+        "facil": Level.facil,
+        "medio": Level.medio,
+        "dificil": Level.dificil,
+        "perito": Level.perito
+      }[this];
+}
+
+extension LevelExt on Level {
+  String get parse => {
+        Level.facil: "facil",
+        Level.medio: " medio",
+        Level.dificil: "dificil",
+        Level.perito: "perito",
+      }[this];
+}
+
+class QuizModel {
+  final String title;
+  final List<QuestionModel> questions;
+  final int questionsAnswered;
+  final String image;
+  final Level level;
+
+  QuizModel(
+      {@required this.title,
+      @required this.questions,
+      @required this.questionsAnswered,
+      @required this.image,
+      @required this.level});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'questions': questions?.map((x) => x.toMap())?.toList(),
+      'questionsAnswered': questionsAnswered,
+      'image': image,
+      'level': level.parse,
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      questions: List<QuestionModel>.from(
+          map['questions']?.map((x) => QuestionModel.fromMap(x))),
+      questionsAnswered: map['questionAnswered'],
+      image: map['image'],
+      level: map["level"].toString().parse,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
+}
---
+++ lib/shared/widgets/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,17 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+
+  ProgressIndicatorWidget(this.value);
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: value,
+      backgroundColor: AppColors.chartSecondary,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+    );
+  }
+}
